2dbd28bd92f528a107d4307d31d37a6081939cfb
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-06 21:24:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,14 @@
 class Student < User
 
-attr_accessor :knowledge
-
-def initialize(knowledge)
-  @knowledge = knowledge
-end
-
-# def learn(string)
-#   @knowledge << string
+# attr_accessor :knowledge
+# 
+# def initialize(knowledge)
+#   @knowledge = knowledge
 # end
+# 
+# # def learn(string)
+# #   @knowledge << string
+# # end
 
 
 end
